3948f43efaba17291c47627b4cc83608dec56534
ramon-84 <ramong_84@hotmail.com>
2020-07-15 18:55:22 -0400

    patron3

---
+++ patron3.rb
@@ -0,0 +1,14 @@
+n = ARGV[0].to_i
+
+n.times do |i|
+    if i%6 == 0 || i%6 == 1
+        print '.'
+    elsif i%6 == 2 || i%6 == 3
+        print '*'
+    else
+        print '|'
+    end
+end
+
+print "\n"
+
